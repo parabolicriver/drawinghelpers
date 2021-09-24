f3260b5f1d418b7c97dc8fb7e7c9f33e5ae08097
Anuj Seth <seth@parabolicriver.com>
2021-09-24 19:29:03 +0530

    Added helpers for iPhone 12, 12 Pro, 12 Pro Max, 13, 13 Pro and 13 Pro Max.

--- UIViewDrawingHelpers.swift
+++ UIViewDrawingHelpers.swift
@@ -31,23 +31,6 @@ import UIKit
     
     /** iPhones **/
     
-    class func isRetina5_42Inch() -> Bool {
-        var isScale3 = false
-        var widthAndHeightMatch = false
-        
-        if (UIView.isRetinaiPhone()) {
-            if (UIScreen.main.scale == 3.0) { isScale3 = true }
-            
-            let screenBounds = UIScreen.main.bounds
-            if ((screenBounds.size.height == kiPhoneRetina5_42Height && screenBounds.size.width == kiPhoneRetina5_42Width) ||
-                (screenBounds.size.height == kiPhoneRetina5_42Width && screenBounds.size.width == kiPhoneRetina5_42Height)) {
-                widthAndHeightMatch = true;
-            }
-        }
-        
-        return isScale3 && widthAndHeightMatch;
-    }
-    
     // iPhone 12 mini, 13 mini
     
     fileprivate static let kiPhoneRetina5_42Width = 375.0
@@ -76,17 +59,114 @@ import UIKit
     class func iPhoneRetina5_42Bounds(landscape: Bool) -> CGRect {
         return CGRect(x: 0.0, y: 0.0, width: iPhoneRetina5_42Width(landscape: landscape), height: iPhoneRetina5_42Height(landscape: landscape));
     }
+    
+    class func isRetina5_42Inch() -> Bool {
+        var isScale3 = false
+        var widthAndHeightMatch = false
+        
+        if (UIView.isRetinaiPhone()) {
+            if (UIScreen.main.scale == 3.0) { isScale3 = true }
+            
+            let screenBounds = UIScreen.main.bounds
+            if ((screenBounds.size.height == kiPhoneRetina5_42Height && screenBounds.size.width == kiPhoneRetina5_42Width) ||
+                (screenBounds.size.height == kiPhoneRetina5_42Width && screenBounds.size.width == kiPhoneRetina5_42Height)) {
+                widthAndHeightMatch = true;
+            }
+        }
+        
+        return isScale3 && widthAndHeightMatch;
+    }
             
     // iPhone 12, 12 Pro, 13, 13 Pro
     
+    fileprivate static let kiPhoneRetina6_06Width = 390.0
+    fileprivate static let kiPhoneRetina6_06Height = 844.0
+    
+    class func iPhoneRetina6_06Width(landscape: Bool) -> CGFloat {
+        if (landscape) {
+            return kiPhoneRetina6_06Height
+        } else {
+            return kiPhoneRetina6_06Width
+        }
+    }
+    
+    class func iPhoneRetina6_06Height(landscape: Bool) -> CGFloat {
+        if (landscape) {
+            return kiPhoneRetina6_06Width
+        } else {
+            return kiPhoneRetina6_06Height
+        }
+    }
+    
+    class func iPhoneRetina6_06ScreenSize(landscape: Bool) -> CGSize {
+        return CGSize(width: iPhoneRetina6_06Width(landscape: landscape), height: iPhoneRetina6_06Height(landscape: landscape));
+    }
+    
+    class func iPhoneRetina6_06Bounds(landscape: Bool) -> CGRect {
+        return CGRect(x: 0.0, y: 0.0, width: iPhoneRetina6_06Width(landscape: landscape), height: iPhoneRetina6_06Height(landscape: landscape));
+    }
+    
     class func isRetina6_06Inch() -> Bool {
-        return false;
+        var isScale3 = false
+        var widthAndHeightMatch = false
+        
+        if (UIView.isRetinaiPhone()) {
+            if (UIScreen.main.scale == 3.0) { isScale3 = true }
+            
+            let screenBounds = UIScreen.main.bounds
+            if ((screenBounds.size.height == kiPhoneRetina6_06Height && screenBounds.size.width == kiPhoneRetina6_06Width) ||
+                (screenBounds.size.height == kiPhoneRetina6_06Width && screenBounds.size.width == kiPhoneRetina6_06Height)) {
+                widthAndHeightMatch = true;
+            }
+        }
+        
+        return isScale3 && widthAndHeightMatch;
     }
     
     // iPhone 12 Pro Max, 13 Pro Max
     
+    fileprivate static let kiPhoneRetina6_68Width = 428.0
+    fileprivate static let kiPhoneRetina6_68Height = 926.0
+    
+    class func iPhoneRetina6_68Width(landscape: Bool) -> CGFloat {
+        if (landscape) {
+            return kiPhoneRetina6_68Height
+        } else {
+            return kiPhoneRetina6_68Width
+        }
+    }
+    
+    class func iPhoneRetina6_68Height(landscape: Bool) -> CGFloat {
+        if (landscape) {
+            return kiPhoneRetina6_68Width
+        } else {
+            return kiPhoneRetina6_68Height
+        }
+    }
+    
+    class func iPhoneRetina6_68ScreenSize(landscape: Bool) -> CGSize {
+        return CGSize(width: iPhoneRetina6_68Width(landscape: landscape), height: iPhoneRetina6_68Height(landscape: landscape));
+    }
+    
+    class func iPhoneRetina6_68Bounds(landscape: Bool) -> CGRect {
+        return CGRect(x: 0.0, y: 0.0, width: iPhoneRetina6_68Width(landscape: landscape), height: iPhoneRetina6_68Height(landscape: landscape));
+    }
+    
     class func isRetina6_68Inch() -> Bool {
-        return false;
+        var isScale3 = false
+        var widthAndHeightMatch = false
+        
+        if (UIView.isRetinaiPhone()) {
+            if (UIScreen.main.scale == 3.0) { isScale3 = true }
+            
+            let screenBounds = UIScreen.main.bounds
+            if ((screenBounds.size.height == kiPhoneRetina6_68Height && screenBounds.size.width == kiPhoneRetina6_68Width) ||
+                (screenBounds.size.height == kiPhoneRetina6_68Width && screenBounds.size.width == kiPhoneRetina6_68Height)) {
+                widthAndHeightMatch = true;
+            }
+        }
+        
+        return isScale3 && widthAndHeightMatch;
     }
 }
 
