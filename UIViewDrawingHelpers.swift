54bb63dcbd9c7ef859f3ccc695c155cda56a61ef
Anuj Seth <seth@parabolicriver.com>
2021-09-24 19:05:45 +0530

    Some work on getting the mini phone helpers up. Need to figure out how to call an objc method from swift next.

--- UIViewDrawingHelpers.swift
+++ UIViewDrawingHelpers.swift
@@ -31,14 +31,45 @@ import UIKit
     
     /** iPhones **/
     
+    class func isRetina5_42Inch() -> Bool {
+        var isScale3 = false
+        var widthAndHeightMatch = false
+        
+        
+        
+        return isScale3 && widthAndHeightMatch;
+    }
+    
     // iPhone 12 mini, 13 mini
     
-    static let iPhoneRetina5_42Width = 375;
-    static let iPhoneRetina5_42Height = 812;
+    fileprivate static let iPhoneRetina5_42Width = 375.0
+    fileprivate static let iPhoneRetina5_42Height = 812.0
     
-    class func isRetina5_42Inch() -> Bool {
-        return false;
+    class func iPhoneRetina5_42Width(landscape: Bool) -> CGFloat {
+        if (landscape) {
+            return iPhoneRetina5_42Height
+        } else {
+            return iPhoneRetina5_42Width
+        }
     }
+    
+    class func iPhoneRetina5_42Height(landscape: Bool) -> CGFloat {
+        if (landscape) {
+            return iPhoneRetina5_42Width
+        } else {
+            return iPhoneRetina5_42Height
+        }
+    
+    }
+    
+    class func iPhoneRetina5_42ScreenSize(landscape: Bool) -> CGSize {
+        return CGSize(width: iPhoneRetina5_42Width(landscape: landscape), height: iPhoneRetina5_42Height(landscape: landscape));
+    }
+    
+    class func iPhoneRetina5_42Bounds(landscape: Bool) -> CGRect {
+        return CGRect(x: 0.0, y: 0.0, width: iPhoneRetina5_42Width(landscape: landscape), height: iPhoneRetina5_42Height(landscape: landscape));
+    }
+        
     
     // iPhone 12, 12 Pro, 13, 13 Pro
     
